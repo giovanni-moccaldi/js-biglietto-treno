f17d5a1a674f92f014967f76d1344176b39f0083
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-23 14:01:05 +0100

    traccia

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert("hello");
+// chiedere quanti km vuoi percorrere
+ 
+//chiedere età  
+// calcolo prezzo del viaggio
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km).
+// se ha meno di 18 anni va applicato uno sconto del 20%
+// se ha piu' di 65 anni va applicato uno sconto del 40%
+// 'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// 
+// 
